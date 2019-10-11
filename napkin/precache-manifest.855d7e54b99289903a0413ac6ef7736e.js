0c35fffc212cec5761d2f93773e4559096606ba7
Rein van der Woerd <reinvanderwoerd@me.com>
2019-10-11 16:14:52 +0200

    napkin

---
+++ napkin/precache-manifest.855d7e54b99289903a0413ac6ef7736e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7a7a01b907a25c3047e",
+    "url": "/css/app.ea86452a.css"
+  },
+  {
+    "revision": "a7a7a01b907a25c3047e",
+    "url": "/js/app.e77fd089.js"
+  },
+  {
+    "revision": "598d17ed588b049820b1",
+    "url": "/js/chunk-vendors.d5ed2577.js"
+  },
+  {
+    "revision": "e834968153f5b42541e463b7a599a356",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
